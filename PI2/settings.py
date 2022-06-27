8b4ef2ae03b70fe37b570ca918a96ce14d018554
Guilherme Valterano Peloi <91574181+gvpeloi@users.noreply.github.com>
2022-06-27 18:45:41 -0300

    1

--- PI2/settings.py
+++ PI2/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,9 +103,8 @@ DATABASES = {
     }
 }
 """
-DATABASES = {
-    'default': dj_database_url()
-}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 

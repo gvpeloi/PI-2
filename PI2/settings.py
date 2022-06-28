fab211812f9fed0922b0c279dc86c4ba12f994a7
Guilherme Valterano Peloi <91574181+gvpeloi@users.noreply.github.com>
2022-06-27 22:10:55 -0300

    favicon

--- PI2/settings.py
+++ PI2/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i7%3@d-q&*77%_@%859%qa$7xx#%#ewb04ra_&u#(a1$e6#8z5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
